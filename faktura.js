1f5bad891e6dcefc3f45e18f8a31891fe59a8af1
Andreas Wallström <user.name>
2019-12-13 13:04:18 +0100

    scss 2.6.2

--- faktura.js
+++ faktura.js
@@ -1,6 +1,6 @@
 // tar bort knapp när man trycker på knappen.
 document.querySelector("#skrivaPDF").addEventListener("click", function () {
-   var pdf = document.getElementById("skrivaPDF");
+    var pdf = document.getElementById("skrivaPDF");
     var tillBaka = document.getElementById("back");
     pdf.style.visibility = 'hidden';
     tillBaka.style.visibility = 'hidden';
